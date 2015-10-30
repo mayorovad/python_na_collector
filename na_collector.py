7129ca588082acf7e897973e7f3833e04df7adde
mayorovad <mayorov.ad@outlook.com>
2015-10-31 01:51:11 +0300

    Порассматривал пакеты

--- na_collector.py
+++ na_collector.py
@@ -1,23 +1,43 @@
-import binascii
-import socket
-import sys
-from struct import *
+import socket,struct,sys,time
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 2463)
 sock.connect(server_address)
 
-
+#Отправляем пакет, который указывает на то, что размер следующего будет 40 (\x28 = 40)
 packed_data = b'\x80\x00\x00\x28'
 
 sock.sendall(packed_data)
 
-#Просто плюем в сервер 40 байт
+#для теста просто закидываю 40 байт из повторяющихся 4-ех -  x80\x00\x00\x28
 packed_data = b'\x80\x00\x00\x28\x80\x00\x00\x28\x80\x00\x00\x28\x80\x00\x00\x28\x80\x00\x00\x28\x80\x00\x00\x28\x80\x00\x00\x28\x80\x00\x00\x28\x80\x00\x00\x28\x80\x00\x00\x28'
-#Он выплевывает что-то большое иногда, а иногда выплевывает 4 байта
+
 sock.sendall(packed_data)
 
-tmp = sock.recv(40000)
-print(binascii.hexlify(tmp))
+#Разобраться, как получать данные пакетами и проверять на конец передачи
+
+'''
+def recvall(sock):
+    data = ""
+    part = None
+    while part != "":
+        part = sock.recv(4096)
+        data += part
+    return data
+
+data = recvall(sock)
+print(data)
+text = data.decode("utf-8")
+
+'''	
+data = sock.recv(4096)					
+print(data)
+data = sock.recv(4096)					
+print(data)
+data = sock.recv(4096)					
+print(data)
+#Чаще всего на тот пакет получаю ответ b'\x80\x00\xe4\xd0'
+#Но иногда выплевывает что-то очень большое, содержащее в себе imno FactoryDefault 
+# и теги SolTPGSALUA и SVC
 print("\n")
